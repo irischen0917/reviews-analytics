2c516eefb274d7af438ba137fcb99ae355053d64
Iris Chen <jin2006613@gmail.com>
2020-10-20 13:58:15 +0800

    add calculation

--- reviews.py
+++ reviews.py
@@ -1,10 +1,18 @@
 #reviews
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
-		print(len(data))
-		
-print(len(data))
-print(data[0])
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d) #sum_len = sume_len + len(d)
+
+print('平均留言長度為：', sum_len / len(data))
 

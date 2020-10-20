58aaf6a056d07b1b4d8fe0a4cde749019c92b731
Iris Chen <jin2006613@gmail.com>
2020-10-20 14:11:28 +0800

    add filter

--- reviews.py
+++ reviews.py
@@ -3,7 +3,7 @@ data = []
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line.strip())
+		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
@@ -15,4 +15,13 @@ for d in data:
 	sum_len += len(d) #sum_len = sume_len + len(d)
 
 print('平均留言長度為：', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有' , len(new), '筆資料長度小於100')
+print(new[0])
+print(new[1])
+
 
